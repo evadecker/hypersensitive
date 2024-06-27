8525c0bf284fc6e346dbfbdfb6fef6aaa913deb7
Eva Decker <itsevadecker@gmail.com>
2024-06-26 21:39:55 -0400

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://hypersensitive.eva.town",
+  devToolbar: {
+    enabled: false,
+  },
+});
